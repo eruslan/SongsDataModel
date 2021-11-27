27b490fefe3699db5dab289b224904df4fc31054
DMV <dmitrilaptev@mail.ru>
2021-11-27 18:44:55 +0300

    feat: ready to submit for the qualiffication

--- etl.py
+++ etl.py
@@ -6,38 +6,88 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """
+    Reads the file in filepath and with help of cur cursor
+    inserts data into songs and artists table. On errors prints errors.
+    Input:
+        cur - cursor
+        filepath - string
+    Return:
+        None
+    Output:
+        info on errors
+    """
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
+
+    # insert artist record
+    artist_data = df.loc[0]["artist_id"], \
+        df.loc[0]["artist_name"], \
+        df.loc[0]["artist_location"], \
+        df.loc[0]["artist_latitude"], \
+        df.loc[0]["artist_longitude"]
+    try:
+        cur.execute(artist_table_insert, artist_data)
+    except psycopg2.Error as e:
+        print("Error: Inserting artist")
+        print(e)
 
     # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
-    
-    # insert artist record
-    artist_data = 
-    cur.execute(artist_table_insert, artist_data)
+    song_data = df.loc[0]["song_id"], \
+        df.loc[0]["title"], \
+        df.loc[0]["artist_id"], \
+        int(df.loc[0]["year"]), \
+        df.loc[0]["duration"]
+    try:
+        cur.execute(song_table_insert, song_data)
+    except psycopg2.Error as e:
+        print("Error: Inserting song")
+        print(e)
 
 
 def process_log_file(cur, filepath):
+    """
+    Reads the file in filepath and with help of cur cursor
+    inserts data into songplays, users and time table. On errors prints errors.
+    Input:
+        cur - cursor
+        filepath - string
+    Return:
+        None
+    Output:
+        Info on errors
+    """
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df.page == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
-    
+    t = pd.to_datetime(df['ts'], unit='ms')
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = pd.concat([df['ts'],
+                           t.dt.hour,
+                           t.dt.day,
+                           t.dt.week,
+                           t.dt.month,
+                           t.dt.year,
+                           t.dt.weekday],
+                          axis=1)
+    column_labels = ["ts", "hour", "day", "week", "month", "year", "weekday"]
+    time_df = time_data.set_axis(column_labels, axis=1, inplace=False)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df.filter(['userId',
+                         'firstName',
+                         'lastName',
+                         'gender',
+                         'level'], axis=1
+                        )
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -45,27 +95,50 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
+            print("No entry in song_log for log_data {} {} {}".format(
+                row.song, row.artist, row.length))
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = row.userId, \
+            songid, \
+            artistid, \
+            row.sessionId, \
+            row.ts, \
+            row.level, \
+            row.location, \
+            row.userAgent
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+    """
+    Enlists JSON files in filepath dirrectory and calls func function against each file.
+    cur cursor is passed to func function to operate with database data. 
+    conn connection is used to commit transactions at the end 
+    Input:
+        cur - cursor
+        conn - database connection
+        filepath - string
+        func - function to call against json files
+    Return:
+        None
+    Output:
+        Some print info about work done
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
@@ -80,7 +153,12 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    """
+    Sets up connection to local sparkifydb. Performs data processing 
+    on songs and logs JSON files
+    """
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
--- sql_queries.py
+++ sql_queries.py
@@ -1,52 +1,131 @@
+"""
+sql_queries.py module is used to determine SQL statements performed against sparkifydb during etl.py import process
+there are DROP TABLES, CREATE TABLES, ISERT TABLES, FIND SONGS SQL statements
+The statements can be used as strings in query lists provided:
+create_table_queries = [songplay_table_create, 
+                        user_table_create,
+                        song_table_create, 
+                        artist_table_create, 
+                        time_table_create]
+drop_table_queries = [songplay_table_drop, 
+                        user_table_drop,
+                        song_table_drop, 
+                        artist_table_drop, 
+                        time_table_drop]
+
+"""
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-""")
+songplay_table_create = "CREATE TABLE IF NOT EXISTS songplays (songplay_id serial PRIMARY KEY, \
+                                                               user_id int, \
+                                                               song_id varchar, \
+                                                               artist_id varchar, \
+                                                               session_id int, \
+                                                               start_time bigint, \
+                                                               level varchar, \
+                                                               location varchar, \
+                                                               user_agent varchar);"
 
-user_table_create = ("""
-""")
 
-song_table_create = ("""
-""")
+user_table_create = "CREATE TABLE IF NOT EXISTS users (user_id varchar PRIMARY KEY, \
+                                                       first_name varchar, \
+                                                       last_name varchar, \
+                                                       gender varchar, \
+                                                       level varchar);"
 
-artist_table_create = ("""
-""")
 
-time_table_create = ("""
-""")
+song_table_create = "CREATE TABLE IF NOT EXISTS songs (song_id varchar  PRIMARY KEY, \
+                                                        title varchar, \
+                                                        artist_id varchar, \
+                                                        year int, \
+                                                        duration float);"
+
+
+artist_table_create = "CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, \
+                                                            name varchar, \
+                                                            location varchar, \
+                                                            latitude float, \
+                                                            longitude float);"
+
+
+time_table_create = "CREATE TABLE IF NOT EXISTS time (start_time bigint, \
+                                                       hour int, \
+                                                       day int, \
+                                                       week int, \
+                                                       month int , \
+                                                       year int , \
+                                                       weekday int);"
+
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
-
-user_table_insert = ("""
-""")
-
-song_table_insert = ("""
-""")
-
-artist_table_insert = ("""
-""")
+songplay_table_insert = "INSERT INTO songplays (user_id, \
+                                                 song_id , \
+                                                 artist_id , \
+                                                 session_id, \
+                                                 start_time, \
+                                                 level, \
+                                                 location, \
+                                                 user_agent) \
+                                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
 
 
-time_table_insert = ("""
-""")
+user_table_insert = "INSERT INTO users (user_id, \
+                                             first_name , \
+                                             last_name , \
+                                             gender, \
+                                             level) \
+                                             VALUES (%s, %s, %s, %s, %s)\
+                        ON CONFLICT (user_id) DO NOTHING;"
+
+
+song_table_insert = "INSERT INTO songs (song_id, \
+                                         title , \
+                                         artist_id , \
+                                         year , \
+                                         duration) \
+                                         VALUES (%s, %s, %s, %s, %s)\
+                    ON CONFLICT (song_id) DO NOTHING;"
+
+
+artist_table_insert = "INSERT INTO artists (artist_id, \
+                                             name, \
+                                             location, \
+                                             latitude, \
+                                             longitude) \
+                                             VALUES (%s, %s, %s, %s, %s)\
+                        ON CONFLICT (artist_id) DO NOTHING;"
+
+
+time_table_insert = "INSERT INTO time (start_time, \
+                                         hour, \
+                                         day, \
+                                         week, \
+                                         month, \
+                                         year, \
+                                         weekday) \
+                                         VALUES (%s, %s, %s, %s, %s, %s, %s);"
+
 
 # FIND SONGS
 
-song_select = ("""
-""")
+song_select = "SELECT s.song_id, s.artist_id \
+                FROM songs s \
+                JOIN artists a ON s.artist_id=a.artist_id \
+                WHERE s.title = (%s) AND a.name = (%s) AND s.duration = (%s);"
+
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [songplay_table_create, user_table_create,
+                        song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop,
+                      song_table_drop, artist_table_drop, time_table_drop]
